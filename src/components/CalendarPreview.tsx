6e0333e2672d925aa5717e339408f54b9dc6eb1e
Rudi Szabo <szrudi@gmail.com>
2021-02-07 11:54:17 +0100

    Add rotate prop just to see it in action

--- src/components/CalendarPreview.tsx
+++ src/components/CalendarPreview.tsx
@@ -8,7 +8,7 @@ const CalendarPreview = () => {
   return (
     <>
       <Page width={210} height={297}>
-        <Calendar type="month" firstDay={currentDate} width={550} top={500} left={40} />
+        <Calendar type="month" firstDay={currentDate} width={550} top={500} left={40} rotate={3} />
       </Page>
     </>
   );
