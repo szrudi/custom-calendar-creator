c8833cba57921b0e231f50c65d980d00e421a783
Rudi Szabo <szrudi@gmail.com>
2021-02-08 15:09:30 +0100

    Remove test styles

--- src/hoc/AsPageElement.tsx
+++ src/hoc/AsPageElement.tsx
@@ -47,9 +47,5 @@ const useStyles = makeStyles<Theme, ElementPlacementProps>({
     width: (props) => props.width,
     height: (props) => props.height,
     transform: (props) => `rotate(${props.rotate}deg)`,
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    color: "white",
   },
 });
