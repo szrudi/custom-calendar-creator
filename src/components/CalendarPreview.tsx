4670f9a6a124d598fb5c2b3de491ae76b2893fb1
Rudi Szabo <szrudi@gmail.com>
2021-02-09 12:54:11 +0100

    remove unused import to fix build

--- src/components/CalendarPreview.tsx
+++ src/components/CalendarPreview.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Page from "./Page";
 import Calendar, { CalendarElementProps } from "./calendar";
-import { startOfMonth } from "date-fns";
 import { daysOfWeek } from "../helpers/Globals";
 
 const CalendarPreview = () => {
