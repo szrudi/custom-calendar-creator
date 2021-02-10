a8142e3e5d42ad1c4ed3cfb429ec857581a93c91
Rudi Szabo <szrudi@gmail.com>
2021-02-10 20:50:00 +0100

    first WIP take on using the planned data structure

--- src/components/CalendarPreview.tsx
+++ src/components/CalendarPreview.tsx
@@ -2,30 +2,125 @@ import React from "react";
 import Page from "./Page";
 import Calendar, { CalendarElementProps } from "./calendar";
 import { useLocale } from "../hooks/useLocale";
+import Content, { ContentElementProps } from "./Content";
 
 const CalendarPreview = () => {
   const [locale] = useLocale();
-
   if (!locale) return null;
 
-  const calendarOptions: CalendarElementProps = {
-    type: "month",
-    firstDay: new Date(),
-    // weekStartsOn: daysOfWeek.Monday,
-    locale: locale!,
-    // locale: nl,
-    // locale: hu,
-    width: 550,
-    top: 450,
-    left: 40,
-    rotate: 3,
-  };
-
+  let pageTemplate = pageTemplates[0];
+  let pageSize = pageSizes.find((ps) => ps.id === pageTemplate.defaultPageSize) ?? pageSizes[0];
   return (
-    <Page width={210} height={297}>
-      <Calendar {...calendarOptions} />
+    <Page width={pageSize.width} height={pageSize.height}>
+      {pageTemplate.elements.map((element) => getPageElement(element, locale))}
     </Page>
   );
 };
 
 export default CalendarPreview;
+
+const getPageElement = (elementOptions: any, locale: Locale | null) => {
+  switch (elementOptions.component) {
+    case "Calendar":
+      const calendarOptions: CalendarElementProps = {
+        type: "month",
+        firstDay: new Date(),
+        // weekStartsOn: daysOfWeek.Monday,
+        locale: locale!,
+        // locale: nl,
+        // locale: hu,
+        width: 550,
+        top: 450,
+        left: 40,
+        rotate: 3,
+      };
+      // const calendarOptions: CalendarElementProps = {
+      //   type: elementOptions.type,
+      //   firstDay: startOfMonth(new Date()),
+      //   ...elementOptions.options,
+      //   ...elementOptions.placement,
+      // };
+      return <Calendar {...calendarOptions} />;
+    case "Content":
+      const contentOptions: ContentElementProps = {
+        layoutId: elementOptions.layout,
+        ...elementOptions.options,
+        ...elementOptions.placement,
+      };
+      return <Content {...contentOptions} />;
+    default:
+      return <></>;
+  }
+};
+
+const pageTemplates = [
+  {
+    id: 1,
+    defaultPageSize: 1,
+    possiblePageSizes: [1, 2],
+    elements: [
+      {
+        component: "Calendar",
+        type: "month",
+        cardinality: 1,
+        variant: "horizontal",
+        /**
+         * Placements are in mm, we can calculate the placements in pixels
+         * using the defaultPageSize data
+         */
+        placement: {
+          top: 160,
+          left: 10,
+          width: 190,
+          height: 117,
+          rotate: 0,
+        },
+        options: {
+          startOfWeek: "0-6",
+          showGrid: true,
+          showWeekNumbers: true,
+          showNameDays: false,
+          showHolidays: true,
+          showCustomEvents: true,
+          showWeekends: true,
+        },
+      },
+      {
+        component: "Content",
+        layout: 1,
+        placement: {
+          top: 10,
+          left: 10,
+          width: 190,
+          height: 150,
+          rotate: 0,
+        },
+        options: {},
+      },
+    ],
+  },
+];
+
+const pageSizes = [
+  {
+    id: 1,
+    name: "A4-vertical",
+    width: 210,
+    height: 297,
+    dpi: 300,
+  },
+  {
+    id: 2,
+    name: "A3",
+    width: 297,
+    height: 420,
+    dpi: 300,
+  },
+  {
+    id: 3,
+    name: "120×180-landscape",
+    width: 180,
+    height: 120,
+    dpi: 300,
+  },
+];
---
+++ src/components/Content.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import asPageElement, { ElementPlacementProps } from "../hoc/AsPageElement";
+
+const Content = (options: ContentProps) => {
+  let layout = layouts.find((layout) => layout.id === options.layoutId) ?? layouts[0];
+  return <div>{layout.name}</div>;
+};
+
+export default asPageElement(Content);
+
+export type ContentProps = {
+  layoutId: number;
+};
+export type ContentElementProps = ContentProps & Partial<ElementPlacementProps>;
+
+const layouts = [
+  {
+    id: 1,
+    name: "Single photo with one text",
+    photoPlaceholders: [
+      {
+        key: "main-photo",
+        placement: {
+          x: "0",
+          y: "0",
+          width: "100%",
+          height: "100%",
+          rotate: "0"
+        }
+      }
+    ],
+    textBoxes: [
+      {
+        key: "month-name",
+        align: "center",
+        value: "{MONTH}",
+        placement: {
+          x: "0",
+          y: "90%",
+          width: "100%",
+          height: "10%",
+          rotate: "0"
+        }
+      }
+    ]
+  }
+];
